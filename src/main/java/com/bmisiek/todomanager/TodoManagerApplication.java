a6ddae2a3b2f4c5a4e39b1501ffa846542fbc076
Indeed <gravitybartek@gmail.com>
2025-03-16 15:30:59 +0100

    add sample controller

--- src/main/java/com/bmisiek/todomanager/TodoManagerApplication.java
+++ src/main/java/com/bmisiek/todomanager/TodoManagerApplication.java
@@ -3,7 +3,7 @@ package com.bmisiek.todomanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bmisiek.todomanager.controller")
 public class TodoManagerApplication {
 
     public static void main(String[] args) {

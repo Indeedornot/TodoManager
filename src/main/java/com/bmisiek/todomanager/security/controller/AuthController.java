10e1e9047478b9a027af6daea9ebbc7494a513c7
Indeed <gravitybartek@gmail.com>
2025-05-25 20:58:41 +0200

    attempt at using views

---
+++ src/main/java/com/bmisiek/todomanager/security/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.bmisiek.todomanager.security.controller;
+
+import com.bmisiek.todomanager.security.entity.User;
+import com.bmisiek.todomanager.security.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.registerUser(user.getUsername(), user.getPassword(), "ROLE_USER");
+        return "redirect:/login";
+    }
+}

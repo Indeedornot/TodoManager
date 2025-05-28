4f30786da131d3ac0d4ffc903128fb1cc5a3826e
Indeed <gravitybartek@gmail.com>
2025-05-28 12:37:18 +0200

    fix flakey tests

--- src/test/java/com/bmisiek/todomanager/integration/config/IntegrationTest.java
+++ src/test/java/com/bmisiek/todomanager/integration/config/IntegrationTest.java
@@ -2,6 +2,7 @@ package com.bmisiek.todomanager.integration.config;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,5 +19,6 @@ import java.lang.annotation.Target;
 @Transactional
 @Testcontainers
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public @interface IntegrationTest {
 }
